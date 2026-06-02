399f80e6e95210f9294752d6c46af4dcbec451ab
KamiGit21 <kamila.jimenez@ucb.edu.bo>
2026-06-01 23:18:16 -0400

    package json and viteconfig agregados

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,                           // FRONTEND PUERTO
+    port: 5174,                           // FRONTEND PUERTO
     host: 'localhost',
     strictPort: true,                     // Falla si el puerto está ocupado
     open: false                           // No abre navegador automáticamente
